6091c6b9c902c35708a64132bfa597b5e9215adf
João Carneiro <joao.carneiro@gmail.com>
2022-07-05 11:14:45 -0300

    feat: Add enemy basic movement AI

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,11 +6,16 @@ using UnityEngine.UI;
 public class Enemy : MonoBehaviour {
 
     public float maxHealth = 30f;
+    public float moveSpeed = 1f;
     public Image healthBar;
     private float health;
+    private Transform target;
+    private Rigidbody2D rb;
 
     void Start() {
         health = maxHealth;
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        rb = GetComponent<Rigidbody2D>();
     }
     
     public void damage(float damage) {
@@ -20,5 +25,9 @@ public class Enemy : MonoBehaviour {
         }
 
         healthBar.fillAmount = health / maxHealth;
+    }
+
+    void Update() {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
     }
 }
