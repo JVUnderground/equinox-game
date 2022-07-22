11d17ac914801236b2b26f271d610a3df426b6ce
Donuts1989 <31610391+Donuts1989@users.noreply.github.com>
2022-07-22 15:44:08 -0300

    feat: adds the enemy spawner

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -8,6 +8,48 @@ public class EnemyManager : MonoBehaviour {
     public GameObject enemyPrefab;
     private float nextActionTime = 0.0f;
     public float period = 1.0f;
+    public GameObject player;
+    
+
+    Vector3 randomVector() {
+
+        int direction = Random.Range(1, 4);
+        float xMax=0, xMin=0, yMax=0, yMin=0;
+        print(direction);
+
+        if (direction == 1) {
+            xMax = player.transform.position.x + 4.5f;
+            xMin = player.transform.position.x - 4.5f;
+            yMax = player.transform.position.y + 4.5f;
+            yMin = player.transform.position.y + 2.5f;
+        }
+    
+        if (direction == 2) {
+            xMax = player.transform.position.x + 6.5f;
+            xMin = player.transform.position.x + 4.5f;
+            yMax = player.transform.position.y + 2.5f;
+            yMin = player.transform.position.y - 2.5f;
+        }
+
+        if (direction == 3) {
+            xMax = player.transform.position.x + 4.5f;
+            xMin = player.transform.position.x - 4.5f;
+            yMax = player.transform.position.y - 2.5f;
+            yMin = player.transform.position.y - 4.5f;
+        }
+
+        if (direction == 4) {
+            xMax = player.transform.position.x - 4.5f;
+            xMin = player.transform.position.x - 6.5f;
+            yMax = player.transform.position.y + 2.5f;
+            yMin = player.transform.position.y - 2.5f;
+        }
+
+        float x = Random.Range(xMin, xMax);
+        float y = Random.Range(yMin, yMax);
+      
+        return new Vector3(x,y,0);
+    }
 
     void Start() {
 
@@ -16,7 +58,7 @@ public class EnemyManager : MonoBehaviour {
     void Update () {
         if (Time.time > nextActionTime ) {
             nextActionTime += period;
-            Instantiate(enemyPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+            Instantiate(enemyPrefab, randomVector(), Quaternion.identity);
         }
     }
 }
