4836504a808af05e80308f4de309482ceddf5871
João Carneiro <joao.carneiro@gmail.com>
2022-07-26 17:43:17 -0300

    feat: Add gem bounce animation

--- Assets/Scripts/Experience.cs
+++ Assets/Scripts/Experience.cs
@@ -5,19 +5,35 @@ using UnityEngine;
 public class Experience : MonoBehaviour {
     // Start is called before the first frame update
 
-    float moveSpeed = 1.5f;
+    float moveSpeed = 3f;
+    float bounceDuration = 0.2f;
+    float bounceTime = float.PositiveInfinity;
+    bool bounced = false;
     bool isClaimed = false;
     GameObject target;
     CircleCollider2D _collider;
+    Rigidbody2D rb;
 
     void Start() {
         _collider = GetComponent<CircleCollider2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update() {
         if (isClaimed && target != null) {
-            transform.position = Vector2.MoveTowards(transform.position, target.transform.position, moveSpeed * Time.deltaTime);
+            if (Time.time > bounceTime) {
+                if (bounced) {
+                    transform.position = Vector2.MoveTowards(transform.position, target.transform.position, moveSpeed * Time.deltaTime);
+                } else {
+                    rb.velocity = Vector3.zero;
+                    bounced = true;
+                }
+                
+            } else {
+                Vector3 force = gameObject.transform.position - target.transform.position;
+                rb.velocity = 3 * force;
+            }   
         }
     }
 
@@ -26,8 +42,9 @@ public class Experience : MonoBehaviour {
             isClaimed = true;
             target = other.gameObject;
             _collider.radius = 0.05f;
+            bounceTime = Time.time + bounceDuration;
         } else if (isClaimed && other.tag == "Player") {
-            Player player = target.GetComponent<Player>();
+            Player player = other.gameObject.GetComponent<Player>();
             player.AddExperience(10);
             Destroy(gameObject);
         }
