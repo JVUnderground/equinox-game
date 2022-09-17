d7311ff89c46b3155fea5e396388c08d7f8c6232
Donuts1989 <31610391+Donuts1989@users.noreply.github.com>
2022-09-17 13:33:43 -0300

    feat: Walls and Goku Character (#15)

---
+++ Assets/Scripts/DestructibleWalls.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructibleWalls : MonoBehaviour
+{
+
+    public Sprite dmgSprite;
+    public int hp = 4;
+
+    private SpriteRenderer spriteRenderer;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void DamageWall(int loss)
+    {
+        spriteRenderer.sprite = dmgSprite;
+        hp -= loss;
+        if (hp <= 0)
+            gameObject.SetActive(false);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,24 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public GameObject crossHair;
-    void Start()
-    {
+   
+    void Start() {
         Cursor.visible = false;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        float mouseX = Input.mousePosition.x;
-        float mouseY = Input.mousePosition.y;
-        Vector3 target = Camera.main.ScreenToWorldPoint(new Vector3(mouseX, mouseY, 0f));
-        target.Set(target.x, target.y, transform.position.z);
-        crossHair.transform.position = target;
-    }
-
+    
     void FixedUpdate()
     {
+        Debug.Log("Hello");
         float moveSpeed = GetComponent<Player>().moveSpeed;
         float horizontalInput = Input.GetAxis("Horizontal");
         //Get the value of the Horizontal input axis.
@@ -31,5 +23,7 @@ public class PlayerController : MonoBehaviour
 
         transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * moveSpeed * Time.deltaTime);
         //Move the object to XYZ coordinates defined as horizontalInput, 0, and verticalInput respectively.
+
     }
 }
+
